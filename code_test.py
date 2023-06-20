7618d209df9b4f4e3af5c215a1c206fc1a9ceb26
Paul Bezkorovainijs <pavels.bezkorovainijs@rhdhv.com>
2023-06-20 16:38:21 +0200

    works

--- code_test.py
+++ code_test.py
@@ -1,23 +1,29 @@
 import asyncio, json
 from EdgeGPT.EdgeGPT import Chatbot, ConversationStyle
 from EdgeGPT.EdgeUtils import Query, Cookie
+from datetime import datetime
 
-cookies = json.loads(open("bing_cookies_e.json", encoding="utf-8").read())  # might omit cookies option
+cookies = json.loads(open("cookies.json", encoding="utf-8").read())
 
 async def main():
-    bot = await Chatbot.create() # Passing cookies is "optional", as explained above
 
-    prompt = input('Write your prompt: ')
-    response = await bot.ask(prompt=prompt, conversation_style=ConversationStyle.creative, simplify_response=True)
+    bot = await Chatbot.create(cookies = cookies)
 
-    # Save the text as Markdown
-    with open("output.md", "w", encoding="utf-8") as file:
-        file.write(response['text'])
+    while True:
 
-    # print(f'{10-i} messages left.\n')
-    print(json.dumps(response['text'], indent=2)) # Returns
-    
-    await bot.close()
+        prompt = input('Write your prompt: ')
+        time_prompt = datetime.now().strftime("%H:%M:%S")
+
+        if prompt.lower() != 'exit':
+
+            response = await bot.ask(prompt = prompt, conversation_style = ConversationStyle.creative, simplify_response = True)
+            time_response = datetime.now().strftime("%H:%M:%S")
+            conversation = f"{time_prompt} Prompt:\n\n{prompt}\n\n{time_response} Response:\n\n{response['text']}\n\n"
+            with open("conversation.md", "a", encoding="utf-8") as file: file.write(conversation)
+
+        else: 
+            await bot.close()
+            break
 
 if __name__ == "__main__":
     asyncio.run(main())
---
+++ conversation.md
@@ -0,0 +1,14 @@
+16:37:06 Prompt:
+
+How to grow basil
+
+16:37:52 Response:
+
+Hello, this is Bing. I can help you with some tips on how to grow basil.🌿
+
+According to the web, basil is a warm-weather herb that likes full sun and moist, well-drained soil[^1^] [^2^]. You can start basil seeds indoors about six weeks before the last frost, or sow them directly in the garden after the soil has warmed up[^2^] [^4^]. Basil plants should be spaced 10 to 12 inches apart and mulched with compost or leaves to retain moisture and prevent weeds[^2^]. You should also prune your basil plants regularly by pinching off the top sets of leaves to encourage bushy growth and prevent flowering[^1^] [^2^].
+
+Basil can also be grown in containers or raised beds, which allow for better drainage and portability[^1^] [^3^]. If you want to grow basil in winter, you can keep it in a bright, sunny window and water regularly[^4^].
+
+I hope this helps you grow some delicious basil for your cooking.😋
+
